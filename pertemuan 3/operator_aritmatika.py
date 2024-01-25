689c738d93a040d7c35bb053bc1c53bf88791527
RestTwoo <144980389+RestTwoo@users.noreply.github.com>
2024-01-25 11:13:33 +0700

    Add files via upload

---
+++ pertemuan 3/operator_aritmatika.py
@@ -0,0 +1,23 @@
+x = 5
+y = 3
+
+#penjumlahan 
+print("penjumlahan =", x + y)
+
+#pengurangan (-)
+print("pengurangan =", x - y)
+
+#perkalian (*)
+print("perkalian =", x * y)
+
+#pembagian (/)
+print("pembagian =", x / y)
+
+#modulus (%)
+print("moodulus =", x % y)
+
+#perpangkatan (**)
+print("perpangkatan =", x ** y)
+
+#pembagian bulat (//)
+print("pembagiqan bulat =", x // y)
---
+++ pertemuan 3/operator_penugasan.py
@@ -0,0 +1,29 @@
+# penjumlahan
+a = 5
+a += 3
+print(a)
+
+# pengurangan
+b = 8
+b -= 4
+print(b)
+
+# perkalian
+c = 7
+c *= 2
+print(c)
+
+# pembagian
+d = 11
+d /= 9
+print(d)
+
+# perpangkatan
+e = 9
+e **= 6
+print(e)
+
+# modulus
+f = 13
+f %= 7
+print(f)
